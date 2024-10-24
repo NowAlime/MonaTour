a6db2e963f9a3f616720cc7ce329a1a5583ae458
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-10-24 15:27:40 +0300

    first-day

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// JavaScript код для взаємодії з елементами сторінки
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Повідомлення надіслано!');
+  });
+  
